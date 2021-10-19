86b42e89d2b90fef8cc29c3c0bdbdee8387d337f
BilalAcikgoz <84044204+BilalAcikgoz@users.noreply.github.com>
2021-10-19 06:55:48 +0300

    Add files via upload

---
+++ Cafe Sipariş Programı/Cafe Sipariş Programı/Cafe Sipariş Programı/SiparisForm.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace Cafe_Sipariş_Programı
+{
+    partial class SiparisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SiparisForm));
+            this.menuGroupBox = new System.Windows.Forms.GroupBox();
+            this.latteArtirmaButton = new System.Windows.Forms.Button();
+            this.mochaArtirmaButton = new System.Windows.Forms.Button();
+            this.americanoAzaltmaButton = new System.Windows.Forms.Button();
+            this.latteAzaltmaButton = new System.Windows.Forms.Button();
+            this.mochaAzaltmaButton = new System.Windows.Forms.Button();
+            this.americanoArtirmaButton = new System.Windows.Forms.Button();
+            this.americanoSayisiLabel = new System.Windows.Forms.Label();
+            this.latteSayisiLabel = new System.Windows.Forms.Label();
+            this.mochaSayisiLabel = new System.Windows.Forms.Label();
+            this.latteLabel = new System.Windows.Forms.Label();
+            this.mochaLabel = new System.Windows.Forms.Label();
+            this.americanoLabel = new System.Windows.Forms.Label();
+            this.siparisiTamamlaButton = new System.Windows.Forms.Button();
+            this.cikisButton = new System.Windows.Forms.Button();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.sifirlaButton = new System.Windows.Forms.Button();
+            this.menuGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuGroupBox
+            // 
+            this.menuGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.menuGroupBox.Controls.Add(this.latteArtirmaButton);
+            this.menuGroupBox.Controls.Add(this.mochaArtirmaButton);
+            this.menuGroupBox.Controls.Add(this.americanoAzaltmaButton);
+            this.menuGroupBox.Controls.Add(this.latteAzaltmaButton);
+            this.menuGroupBox.Controls.Add(this.mochaAzaltmaButton);
+            this.menuGroupBox.Controls.Add(this.americanoArtirmaButton);
+            this.menuGroupBox.Controls.Add(this.americanoSayisiLabel);
+            this.menuGroupBox.Controls.Add(this.latteSayisiLabel);
+            this.menuGroupBox.Controls.Add(this.mochaSayisiLabel);
+            this.menuGroupBox.Controls.Add(this.latteLabel);
+            this.menuGroupBox.Controls.Add(this.mochaLabel);
+            this.menuGroupBox.Controls.Add(this.americanoLabel);
+            this.menuGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.menuGroupBox.Name = "menuGroupBox";
+            this.menuGroupBox.Size = new System.Drawing.Size(277, 218);
+            this.menuGroupBox.TabIndex = 0;
+            this.menuGroupBox.TabStop = false;
+            this.menuGroupBox.Text = "MENÜ";
+            // 
+            // latteArtirmaButton
+            // 
+            this.latteArtirmaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.latteArtirmaButton.Location = new System.Drawing.Point(176, 92);
+            this.latteArtirmaButton.Name = "latteArtirmaButton";
+            this.latteArtirmaButton.Size = new System.Drawing.Size(26, 28);
+            this.latteArtirmaButton.TabIndex = 7;
+            this.latteArtirmaButton.Text = "+";
+            this.latteArtirmaButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.latteArtirmaButton.UseVisualStyleBackColor = true;
+            this.latteArtirmaButton.Click += new System.EventHandler(this.latteArtirmaButton_Click);
+            // 
+            // mochaArtirmaButton
+            // 
+            this.mochaArtirmaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mochaArtirmaButton.Location = new System.Drawing.Point(176, 141);
+            this.mochaArtirmaButton.Name = "mochaArtirmaButton";
+            this.mochaArtirmaButton.Size = new System.Drawing.Size(26, 28);
+            this.mochaArtirmaButton.TabIndex = 8;
+            this.mochaArtirmaButton.Text = "+";
+            this.mochaArtirmaButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.mochaArtirmaButton.UseVisualStyleBackColor = true;
+            this.mochaArtirmaButton.Click += new System.EventHandler(this.mochaArtirmaButton_Click);
+            // 
+            // americanoAzaltmaButton
+            // 
+            this.americanoAzaltmaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.americanoAzaltmaButton.Location = new System.Drawing.Point(221, 42);
+            this.americanoAzaltmaButton.Name = "americanoAzaltmaButton";
+            this.americanoAzaltmaButton.Size = new System.Drawing.Size(26, 28);
+            this.americanoAzaltmaButton.TabIndex = 9;
+            this.americanoAzaltmaButton.Text = "-";
+            this.americanoAzaltmaButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.americanoAzaltmaButton.UseVisualStyleBackColor = true;
+            this.americanoAzaltmaButton.Click += new System.EventHandler(this.americanoAzaltmaButton_Click);
+            // 
+            // latteAzaltmaButton
+            // 
+            this.latteAzaltmaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.latteAzaltmaButton.Location = new System.Drawing.Point(221, 92);
+            this.latteAzaltmaButton.Name = "latteAzaltmaButton";
+            this.latteAzaltmaButton.Size = new System.Drawing.Size(26, 28);
+            this.latteAzaltmaButton.TabIndex = 10;
+            this.latteAzaltmaButton.Text = "-";
+            this.latteAzaltmaButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.latteAzaltmaButton.UseVisualStyleBackColor = true;
+            this.latteAzaltmaButton.Click += new System.EventHandler(this.latteAzaltmaButton_Click);
+            // 
+            // mochaAzaltmaButton
+            // 
+            this.mochaAzaltmaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mochaAzaltmaButton.Location = new System.Drawing.Point(221, 141);
+            this.mochaAzaltmaButton.Name = "mochaAzaltmaButton";
+            this.mochaAzaltmaButton.Size = new System.Drawing.Size(26, 28);
+            this.mochaAzaltmaButton.TabIndex = 11;
+            this.mochaAzaltmaButton.Text = "-";
+            this.mochaAzaltmaButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.mochaAzaltmaButton.UseVisualStyleBackColor = true;
+            this.mochaAzaltmaButton.Click += new System.EventHandler(this.mochaAzaltmaButton_Click);
+            // 
+            // americanoArtirmaButton
+            // 
+            this.americanoArtirmaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.americanoArtirmaButton.Location = new System.Drawing.Point(176, 42);
+            this.americanoArtirmaButton.Name = "americanoArtirmaButton";
+            this.americanoArtirmaButton.Size = new System.Drawing.Size(26, 28);
+            this.americanoArtirmaButton.TabIndex = 6;
+            this.americanoArtirmaButton.Text = "+";
+            this.americanoArtirmaButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.americanoArtirmaButton.UseVisualStyleBackColor = true;
+            this.americanoArtirmaButton.Click += new System.EventHandler(this.americanoArtirmaButton_Click);
+            // 
+            // americanoSayisiLabel
+            // 
+            this.americanoSayisiLabel.AutoSize = true;
+            this.americanoSayisiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.americanoSayisiLabel.Location = new System.Drawing.Point(134, 42);
+            this.americanoSayisiLabel.Name = "americanoSayisiLabel";
+            this.americanoSayisiLabel.Size = new System.Drawing.Size(16, 18);
+            this.americanoSayisiLabel.TabIndex = 3;
+            this.americanoSayisiLabel.Text = "0";
+            // 
+            // latteSayisiLabel
+            // 
+            this.latteSayisiLabel.AutoSize = true;
+            this.latteSayisiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.latteSayisiLabel.Location = new System.Drawing.Point(134, 96);
+            this.latteSayisiLabel.Name = "latteSayisiLabel";
+            this.latteSayisiLabel.Size = new System.Drawing.Size(16, 18);
+            this.latteSayisiLabel.TabIndex = 4;
+            this.latteSayisiLabel.Text = "0";
+            // 
+            // mochaSayisiLabel
+            // 
+            this.mochaSayisiLabel.AutoSize = true;
+            this.mochaSayisiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mochaSayisiLabel.Location = new System.Drawing.Point(134, 145);
+            this.mochaSayisiLabel.Name = "mochaSayisiLabel";
+            this.mochaSayisiLabel.Size = new System.Drawing.Size(16, 18);
+            this.mochaSayisiLabel.TabIndex = 5;
+            this.mochaSayisiLabel.Text = "0";
+            // 
+            // latteLabel
+            // 
+            this.latteLabel.AutoSize = true;
+            this.latteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.latteLabel.Location = new System.Drawing.Point(6, 96);
+            this.latteLabel.Name = "latteLabel";
+            this.latteLabel.Size = new System.Drawing.Size(40, 18);
+            this.latteLabel.TabIndex = 1;
+            this.latteLabel.Text = "Latte";
+            // 
+            // mochaLabel
+            // 
+            this.mochaLabel.AutoSize = true;
+            this.mochaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mochaLabel.Location = new System.Drawing.Point(6, 145);
+            this.mochaLabel.Name = "mochaLabel";
+            this.mochaLabel.Size = new System.Drawing.Size(54, 18);
+            this.mochaLabel.TabIndex = 2;
+            this.mochaLabel.Text = "Mocha";
+            // 
+            // americanoLabel
+            // 
+            this.americanoLabel.AutoSize = true;
+            this.americanoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.americanoLabel.Location = new System.Drawing.Point(6, 42);
+            this.americanoLabel.Name = "americanoLabel";
+            this.americanoLabel.Size = new System.Drawing.Size(79, 18);
+            this.americanoLabel.TabIndex = 0;
+            this.americanoLabel.Text = "Americano";
+            // 
+            // siparisiTamamlaButton
+            // 
+            this.siparisiTamamlaButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.siparisiTamamlaButton.AutoSize = true;
+            this.siparisiTamamlaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.siparisiTamamlaButton.Image = ((System.Drawing.Image)(resources.GetObject("siparisiTamamlaButton.Image")));
+            this.siparisiTamamlaButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.siparisiTamamlaButton.Location = new System.Drawing.Point(160, 260);
+            this.siparisiTamamlaButton.Name = "siparisiTamamlaButton";
+            this.siparisiTamamlaButton.Size = new System.Drawing.Size(188, 30);
+            this.siparisiTamamlaButton.TabIndex = 2;
+            this.siparisiTamamlaButton.Text = "SİPARİŞİ TAMAMLA";
+            this.siparisiTamamlaButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.siparisiTamamlaButton.UseVisualStyleBackColor = true;
+            this.siparisiTamamlaButton.Click += new System.EventHandler(this.siparisiTamamlaButton_Click);
+            // 
+            // cikisButton
+            // 
+            this.cikisButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cikisButton.AutoSize = true;
+            this.cikisButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikisButton.Image = ((System.Drawing.Image)(resources.GetObject("cikisButton.Image")));
+            this.cikisButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.cikisButton.Location = new System.Drawing.Point(384, 260);
+            this.cikisButton.Name = "cikisButton";
+            this.cikisButton.Size = new System.Drawing.Size(79, 30);
+            this.cikisButton.TabIndex = 3;
+            this.cikisButton.Text = "ÇIKIŞ";
+            this.cikisButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cikisButton.UseVisualStyleBackColor = true;
+            this.cikisButton.Click += new System.EventHandler(this.cikisButton_Click);
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.logoPictureBox.Image = global::Cafe_Sipariş_Programı.Properties.Resources.starbucks_logo;
+            this.logoPictureBox.Location = new System.Drawing.Point(304, 26);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(159, 205);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logoPictureBox.TabIndex = 4;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // sifirlaButton
+            // 
+            this.sifirlaButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sifirlaButton.AutoSize = true;
+            this.sifirlaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sifirlaButton.Image = ((System.Drawing.Image)(resources.GetObject("sifirlaButton.Image")));
+            this.sifirlaButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.sifirlaButton.Location = new System.Drawing.Point(23, 260);
+            this.sifirlaButton.Name = "sifirlaButton";
+            this.sifirlaButton.Size = new System.Drawing.Size(100, 30);
+            this.sifirlaButton.TabIndex = 1;
+            this.sifirlaButton.Text = "SIFIRLA";
+            this.sifirlaButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.sifirlaButton.UseVisualStyleBackColor = true;
+            this.sifirlaButton.Click += new System.EventHandler(this.sifirlaButton_Click);
+            // 
+            // SiparisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 311);
+            this.Controls.Add(this.logoPictureBox);
+            this.Controls.Add(this.cikisButton);
+            this.Controls.Add(this.siparisiTamamlaButton);
+            this.Controls.Add(this.sifirlaButton);
+            this.Controls.Add(this.menuGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 350);
+            this.MinimumSize = new System.Drawing.Size(476, 317);
+            this.Name = "SiparisForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Starbucks Coffee Company";
+            this.menuGroupBox.ResumeLayout(false);
+            this.menuGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox menuGroupBox;
+        private System.Windows.Forms.Button latteArtirmaButton;
+        private System.Windows.Forms.Button mochaArtirmaButton;
+        private System.Windows.Forms.Button americanoAzaltmaButton;
+        private System.Windows.Forms.Button latteAzaltmaButton;
+        private System.Windows.Forms.Button mochaAzaltmaButton;
+        private System.Windows.Forms.Button americanoArtirmaButton;
+        private System.Windows.Forms.Label americanoSayisiLabel;
+        private System.Windows.Forms.Label latteSayisiLabel;
+        private System.Windows.Forms.Label mochaSayisiLabel;
+        private System.Windows.Forms.Label latteLabel;
+        private System.Windows.Forms.Label mochaLabel;
+        private System.Windows.Forms.Label americanoLabel;
+        private System.Windows.Forms.Button sifirlaButton;
+        private System.Windows.Forms.Button siparisiTamamlaButton;
+        private System.Windows.Forms.Button cikisButton;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+    }
+}
+
---
+++ Cafe Sipariş Programı/Cafe Sipariş Programı/Cafe Sipariş Programı/SiparisForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cafe_Sipariş_Programı
+{
+    public partial class SiparisForm : Form
+    {
+        byte americano = 0;
+        byte latte = 0;
+        byte mocha = 0;
+
+        public SiparisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void americanoArtirmaButton_Click(object sender, EventArgs e)
+        {
+            americano++;
+            americanoSayisiLabel.Text = Convert.ToString(americano);
+        }
+
+        private void americanoAzaltmaButton_Click(object sender, EventArgs e)
+        {
+            if (americano > 0)
+            {
+                americano--;
+                americanoSayisiLabel.Text = Convert.ToString(americano);
+            }    
+        }
+
+        private void latteArtirmaButton_Click(object sender, EventArgs e)
+        {
+            latte++;
+            latteSayisiLabel.Text = Convert.ToString(latte);
+        }
+
+        private void latteAzaltmaButton_Click(object sender, EventArgs e)
+        {
+            if (latte > 0)
+            {
+                latte--;
+                latteSayisiLabel.Text = Convert.ToString(latte);
+            }
+        }
+
+        private void mochaArtirmaButton_Click(object sender, EventArgs e)
+        {
+            mocha++;
+            mochaSayisiLabel.Text = Convert.ToString(mocha);
+        }
+
+        private void mochaAzaltmaButton_Click(object sender, EventArgs e)
+        {
+            if (mocha > 0)
+            {
+                mocha--;
+                mochaSayisiLabel.Text = Convert.ToString(mocha);
+            }
+        }
+
+        private void sifirlaButton_Click(object sender, EventArgs e)
+        {
+            americano = 0;
+            latte = 0;
+            mocha = 0;
+            americanoSayisiLabel.Text = "0";
+            latteSayisiLabel.Text = "0";
+            mochaSayisiLabel.Text = "0";
+        }
+
+        private void siparisiTamamlaButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("SİPARİŞ ÖZETİ: " + americanoSayisiLabel.Text + " Americano " + latteSayisiLabel.Text + " Latte " + mochaSayisiLabel.Text + " Mocha");
+        }
+
+        private void cikisButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
